65a202d1aa95cb04feb9c02d514c01481d111187
Costa Robe <56robik@gmail.com>
2026-03-15 12:57:00 +0100

    ok

--- src/ParzysteFor.java
+++ src/ParzysteFor.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
+/**
+ * 8. Używając instrukcji for() napisz program ParzysteFor, który wypisze liczby
+ * parzyste z przedziału od 2 do wartości podanej przez użytkownika.
+ */
 public class ParzysteFor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę: ");
+        int liczba = scanner.nextInt();
+        for(int i = 2; !(liczba < i); i += 2){
+            System.out.println("i = " + i);
+        }
+    }
 }
